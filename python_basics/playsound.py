c3a97e23c687b27e75486785dcfd4416b408b5a9
Tim Schippers <schippers1998@gmail.com>
2023-09-11 14:44:17 +0200

    modified playsound.py to repeat an asked amount of times

--- python_basics/playsound.py
+++ python_basics/playsound.py
@@ -1,7 +1,10 @@
 import simpleaudio as sa
 
-wave_obj = sa.WaveObject.from_wave_file("/Users/tim/Documents/Spaces/Resources/Sounds/Drums/Analog/Kick/MFB_Kick/wa_tanzbar_kick_17.wav") # copy your path to a wav file here
-play_obj = wave_obj.play()
-play_obj.wait_done()
+n = int(input('how many times do you want to hear it:'))
 
-#' found the code on https://pypi.org/project/simpleaudio/
+for i in range(n):
+    wave_obj = sa.WaveObject.from_wave_file("/Users/tim/Documents/Spaces/Resources/Sounds/Drums/Analog/Kick/MFB_Kick/wa_tanzbar_kick_17.wav") # copy your path to a wav file here
+    play_obj = wave_obj.play()
+    play_obj.wait_done()
+
+#' used the code on https://pypi.org/project/simpleaudio/
