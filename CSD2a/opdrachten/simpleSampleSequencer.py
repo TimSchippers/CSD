5422ea96cfd77656379a435df9f514bcdad5a28f
Tim Schippers <schippers1998@gmail.com>
2023-10-02 15:57:12 +0200

    simpleSampleSequencer.py done

--- CSD2a/opdrachten/simpleSampleSequencer.py
+++ CSD2a/opdrachten/simpleSampleSequencer.py
@@ -1,6 +1,5 @@
 import simpleaudio as sa
 import time
-from pynput import keyboard
 
 sequence = []
 durNotes = []
@@ -14,13 +13,10 @@ while (not correctInput):
     if not userBpm:
         # empty string --> use default
         correctInput = True
-    elif bpm < 0:
-       print("No")
     else:
         try:
-            bpm = float(user_bpm)
+            bpm = float(userBpm)
             correctInput = True
-
         except:
             print("No")
 
@@ -32,25 +28,57 @@ sixteenthNoteDur = 15/bpm
 
         
 
-#TODO loops?
 
-#DONE vraag ritme met kwarten als 1
+# vraag ritme met kwarten als 1
 print('1 = quarter note, 0.5 = eighth note etc. etc.\n Press "q" to commit')
 while True:
     try:
-        durNotes = input("note length " + str(len(sequence)) + " : ")
+        durNotes = input("note length " + str(len(sequence)+1) + " : ").replace(",",".")
         if (durNotes == "q"):
             break
         sequence.append(float(durNotes))
+        print(sequence)
     except ValueError:
         print("No")
 
 print(sequence)
 
-#TODO zet ritme om in timestamps van 16de
+# zet ritme om in timestamps van 16de
+
+timestampeSeq = []
+sum = 0
+
+for timestamp in sequence:
+    sixteenthNoteValue = timestamp *4
+    timestampeSeq.append(sum)
+    sum += sixteenthNoteValue
+
+print(timestampeSeq)
+
+# zet timestamps als 16e om naar tijd
+
+timeDurations = [] 
+for noteDur in timestampeSeq:
+    timeDurations.append(sixteenthNoteDur*noteDur)
+
+print(timeDurations)
 
 
 
-#TODO zet timestamps als 16e om naar tijd
+# speel af
+timeZero = time.time()
+print("time zero = ", timeZero)
+wave_obj = sa.WaveObject.from_wave_file("/Users/tim/Documents/Spaces/Resources/Sounds/Drums/Analog/Kick/MFB_Kick/wa_tanzbar_kick_17.wav") # copy your path to a wav file here
 
-#TODO speel af
+
+timePlayed = 0
+
+while True:
+    now = time.time() - timeZero
+    if(timePlayed > (len(timeDurations)-1)):
+        break
+    if(now >= timeDurations[timePlayed]):
+        play_obj = wave_obj.play()
+        timePlayed += 1
+    time.sleep(0.001)
+play_obj.wait_done()
