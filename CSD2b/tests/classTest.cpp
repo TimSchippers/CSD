30d8ef0c8a5fb61629b2f16342c2e3af222ba629
TimSchippers <tim_schippers@icloud.com>
2023-11-18 09:17:10 +0100

    modified instrument class to have intput

--- CSD2b/tests/classTest.cpp
+++ CSD2b/tests/classTest.cpp
@@ -2,19 +2,23 @@
 #include <string>
 
 class Instrument {
-  std::string materialSoundSource = "string";
-  std::string exciter = "bow";
+public:
+  Instrument(std::string material, std::string iExciter) {
+    materialSoundSource = material;
+    exciter = iExciter;
+  }
+  std::string materialSoundSource;
+  std::string exciter;
   int numSoundSources = 1;
 
-public:
   void play() {
     if (materialSoundSource == "string" && exciter == "bow") {
       std::cout << "bowed string sound" << std::endl;
     }
-  };
+  }
 };
 
 int main() {
-  Instrument violin;
+  Instrument violin("string", "bow");
   violin.play();
-};
+}
