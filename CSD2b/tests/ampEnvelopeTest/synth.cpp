4ed54ae8d3f3b7199303de2f190d8d6cf15b4cac
TimSchippers <tim_schippers@icloud.com>
2023-12-08 15:50:52 +0100

    made a simple sine synth in one cpp for fast testing

--- CSD2b/tests/ampEnvelopeTest/synth.cpp
+++ CSD2b/tests/ampEnvelopeTest/synth.cpp
@@ -1,28 +1,41 @@
+#include <cmath>
 class Synth {
 public:
-    Synth(){};
+    Synth():sampleStep(0),samplerate(44100),frequency(220),amplitude(1),phase(0),sample(0){};
     ~Synth(){};
 
     void setSamplerate(int samplerate){this->samplerate = samplerate;}
+    int getSamplerate(){return samplerate;}
     float getSample(){return sample;}
     void tick()
     {
+        sampleStep+= sampleSize;
+        if(sampleStep >= samplerate){
+            sampleStep -= samplerate;
+            seconds += 1;
+        }
         phase += frequency / samplerate;
         if(phase > 1.0f) {
             phase -= 1.0f;
-            }
-        waveSample = 0; // calculate from oscillator
-        sample = 0; // calculate from amplifier
+        }
+
+        waveSample = sin(pi*2*phase); // calculate from oscillator
+        sample = waveSample*amplitude; // calculate from amplifier
     }
 
     //make oscillator
     //make amplifier with envelope
 
 private:
+    int seconds;
+    float sampleStep;
     float samplerate;
+    float sampleSize = 1/samplerate;
     float sample;
     float phase;
     float frequency;
+    float amplitude;
     float waveSample;
+    const float pi = acos(-1);
 
 };
