0fca250b7e138f6b98a2f083e2815b49221668d4
TimSchippers <tim_schippers@icloud.com>
2023-12-08 16:08:00 +0100

    added attack envelope

--- CSD2b/tests/ampEnvelopeTest/synth.cpp
+++ CSD2b/tests/ampEnvelopeTest/synth.cpp
@@ -1,12 +1,23 @@
 #include <cmath>
 class Synth {
 public:
-    Synth():sampleStep(0),samplerate(44100),frequency(220),amplitude(1),phase(0),sample(0){};
+    Synth():sampleStep(0),samplerate(44100),sample(0),phase(0),frequency(220),amplitude(1),attack(0.5){};
     ~Synth(){};
 
     void setSamplerate(int samplerate){this->samplerate = samplerate;}
     int getSamplerate(){return samplerate;}
     float getSample(){return sample;}
+    float calculateSine(float phase){
+        sample = sin(pi*2*phase)*amplitude;
+        return sample;
+    }
+    float calculateEnvelope(float waveSample, float attack, float time){
+        sample = waveSample * time / attack;
+        if(time >= attack){
+            sample = waveSample;
+        }
+        return sample;
+    }
     void tick()
     {
         sampleStep+= sampleSize;
@@ -14,13 +25,14 @@ public:
             sampleStep -= samplerate;
             seconds += 1;
         }
+        time = seconds + sampleStep;
         phase += frequency / samplerate;
         if(phase > 1.0f) {
             phase -= 1.0f;
         }
 
-        waveSample = sin(pi*2*phase); // calculate from oscillator
-        sample = waveSample*amplitude; // calculate from amplifier
+        waveSample = calculateSine(phase);// calculate from oscillator
+        sample = calculateEnvelope(waveSample,attack,time); // calculate from amplifier
     }
 
     //make oscillator
@@ -36,6 +48,8 @@ private:
     float frequency;
     float amplitude;
     float waveSample;
+    float time;
+    float attack;
     const float pi = acos(-1);
 
 };
